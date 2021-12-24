45508e601b162ddb16bf8683b7cd09646b4404cc
Faizal Ismail <96164880+faizalizm@users.noreply.github.com>
2021-12-24 22:32:00 +0800

    main

---
+++ main.cpp
@@ -0,0 +1,83 @@
+/*	
+		|------------------------------------------------------------------
+		|	Program Developed and Maintained w/ ❤  :-
+		|	DeadLine Chasers | DLC
+		|------------------------------------------------------------------
+		| Subject : CSEB324 Data Structure & Algorithms
+		| 1. Name : Muhammad Daniel Bin Mohd Hamdan					ID: SW0107514
+		| 2. Name : Muhammad Faizal 												ID: SW0107290
+		| 3. Name : Muadz Bin Zainuddin											ID: SW0107478
+		| 4. Name : Nur Rahimatil Ain Bt Mat Rahim					ID: SW0107489
+		| 5. Name : Tariq Ahmed Mansour Mohsen Abohatem			ID: SW0106917
+		| 6. Name : Zafar Muhammad Aqif Bin Muhammad Farid	ID: SW0107316
+		|------------------------------------------------------------------
+		|	Works Best on the IDE :-
+		|	Replit.com
+		| https://replit.com/join/lyyjjrpkzr-pejal
+		|------------------------------------------------------------------
+*/	
+
+#include "myCovid.h"
+
+int main() {
+	Malaysia* my = new Malaysia(); // create instance of struct
+	MyCovid mcd(my); // create class object, pass the struct instance for class constructer
+	vector<State> StateList;
+	State s;
+	int choice;
+
+	// header
+	cout << "\n====================================================  ❤\n"
+		<< "\n-= Welcome to Malaysia's Covid-19 Record Database =-\n"
+		<< "\n====================================================  ❤\n"
+		<< "\n  Legend :-"
+		<< "\n  -----------------------------------------------"
+		<< "\n  ->\t\t\t\t\tdenotes an Input/Output"
+		<< "\n  -----------------------------------------------"
+		<< "\n  [1][2][3][4]\t\t\tdenotes Number Choices"
+		<< "\n  -----------------------------------------------"
+		<< "\n  -= !! Msg !! =-\t\tdenotes a Notice"
+		<< "\n  -----------------------------------------------\n"
+		<< "\n====================================================  ❤";
+	
+	do {
+		choice = mcd.menu();
+		
+		switch (choice) {
+		  case 1 : {
+				Record* aRec = new Record();
+				mcd.newRecord(my, aRec);
+				break;
+			}
+    	case 2 :
+				mcd.report(my);
+				break;
+			case 3 :
+				cout << "\n\t-= !! Entering Analyzation Menu !! =-\n";
+				mcd.analysis(&StateList, s, my);
+				break;
+			case 4 :
+				cout << "\n\t-= !! Exiting !! =-\n";
+				break;
+		}
+		
+		if (choice > 4 || choice < 1)
+			cout << "\n\t-= !! Error : Value Out of Bound !! =-";
+			
+	} while (choice != 4);
+
+	// footer
+	cout << "\n====================================================  ❤"
+		<< "\n\n Thank you and remember to keep social distancing !   \n"
+		<< "\n            _o_        6 feet        _o_                  "
+		<< "\n             |      <---------->      |                   "
+		<< "\n             /\\                       /\\              \n"
+		<< "\n====================================================  ❤\n"
+		<< "\n    © 2021 MyCovid Database All Rights Reserved           "
+		<< "\n      -----------------------------------------           "
+		<< "\n      Program Developed and Maintained w/ ❤  :-          "
+		<< "\n      DeadLine Chasers | DLC \n                           "
+		<< "\n====================================================  ❤\n";
+
+  return 0;
+}
